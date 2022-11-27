eb64cd19aaff9fa2cca0ec5b78c6b4e7904b4a2b
Andrew <italkpower@mail.ru>
2022-11-27 15:36:34 +0300

    lessonFour

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Colors } from "./Colors";
+import Steps from "./Steps";
 
 const App = () => {
   return (
     <>
-      <Colors />
+      {/* <Colors /> */}
+      <Steps />
     </>
   );
 };
